227947f70ed4717230aa86aee930bb369618ba5c
RonaldCook <34778211+RonaldCook@users.noreply.github.com>
2017-12-28 11:00:30 -0800

    Refactoring, creating a character state at the parent level and passing it down

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import Constants from './constants.js'
 import './App.css';
 
 var config = require('./config')
@@ -8,30 +9,104 @@ class App extends Component {
   constructor (props) {
     super(props);
 
+    this.state = {
+      character: {
+        stats: [
+          {
+            key: 'STR',
+            name: 'Strength',
+            displayValue: Constants.startingValue,
+            pointBuyValue: Constants.startingValue
+          },
+          {
+            key: 'DEX',
+            name: 'Dexterity',
+            displayValue: Constants.startingValue,
+            pointBuyValue: Constants.startingValue
+          },
+          {
+            key: 'CON',
+            name: 'Constitution',
+            displayValue: Constants.startingValue,
+            pointBuyValue: Constants.startingValue
+          },
+          {
+            key: 'INT',
+            name: 'Intelligence',
+            displayValue: Constants.startingValue,
+            pointBuyValue: Constants.startingValue
+          },
+          {
+            key: 'WIS',
+            name: 'Wisdom',
+            displayValue: Constants.startingValue,
+            pointBuyValue: Constants.startingValue
+          },
+          {
+            key: 'CHA',
+            name: 'Charisma',
+            displayValue: Constants.startingValue,
+            pointBuyValue: Constants.startingValue
+          }
+        ]
+      }
+    };
+
+    this.modifyAttribute = this.modifyAttribute.bind(this);
+
     // Initialize Firebase
     firebase.initializeApp(config.firebase);
   }
 
+  modifyAttribute(key, changeValue) {
+    let character = Object.assign({}, this.state.character);
+    var index = character.stats.findIndex(obj => obj.key === key);
+
+    var stat = character.stats[index];
+    stat.pointBuyValue = (stat.pointBuyValue + changeValue);
+    stat.displayValue = (stat.displayValue + changeValue);
+
+    this.setState({character: character});
+  }
+
   render() {
+    const displayStats = this.state.character.stats.map((data, index) => {
+      return (
+        <td key={index}>
+          <div>
+            <label>{data.name}: </label><input value={data.displayValue} readOnly />
+          </div>
+          <div>
+            <button disabled={true}>+</button>
+            <button disabled={true}>-</button>
+          </div>
+        </td>
+      );
+    });
+
     return (
-      <Builder />
+      <div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                {displayStats}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        &nbsp;
+        <PointBuy character={this.state.character} modifyAttribute={this.modifyAttribute}/>
+      </div>
     );
   }
 }
 
 class Stat extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: this.props.startingValue
-    };
-  }
-
   getPointCost(newValue) {
-    const oldValue = this.state.value;
+    const oldValue = this.props.value;
 
-    if(newValue >= this.props.startingValue && newValue <= this.props.maxValue) {
+    if(newValue >= Constants.startingValue && newValue <= Constants.maxValue) {
       if(oldValue < newValue) {
         return (newValue <= 13 ? 1 : 2);
       } else if(oldValue > newValue) {
@@ -42,95 +117,102 @@ class Stat extends Component {
   }
 
   addToAttributeScore() {
-    const currentValue = this.state.value;
-    const maxValue = this.props.maxValue;
+    const currentValue = this.props.value;
     const currentPoints = this.props.currentPoints;
+    const maxValue = Constants.maxValue;
 
     var pointCost = this.getPointCost(currentValue + 1);
 
     if((pointCost > 0) && (currentPoints >= pointCost) && (currentValue < maxValue)) {
-      var newValue = (currentValue + 1);
-      this.props.updateCurrentPoints(true, pointCost);
-
-      this.setState({
-        value: newValue
-      });
+      this.props.updateCurrentPoints((pointCost * -1));
+      this.props.modifyAttribute(this.props.statKey, 1);
     }
   }
 
   removeFromAttributeScore() {
-    const currentValue = this.state.value;
-    const startingPoints = this.props.startingPoints;
-    const startingValue = this.props.startingValue;
+    const currentValue = this.props.value;
     const currentPoints = this.props.currentPoints;
+    const startingPoints = Constants.startingPoints;
+    const startingValue = Constants.startingValue;
 
     var pointCost = this.getPointCost(currentValue - 1);
 
     if((currentPoints < startingPoints) && (currentValue > startingValue) && (pointCost > 0)) {
-      var newValue = (currentValue - 1);
-      this.props.updateCurrentPoints(false, pointCost);
-
-      this.setState({
-        value: newValue
-      });
+      this.props.updateCurrentPoints(pointCost);
+      this.props.modifyAttribute(this.props.statKey, -1);
     }
   }
 
+  disableAddToScore() {
+    const value = this.props.value;
+    const currentPoints = this.props.currentPoints;
+    const maxValue = Constants.maxValue;
+
+    if((value >= 13 && currentPoints < 2) || (currentPoints <= 0) || (value >= maxValue)) {
+      return "disabled";
+    }
+    return "";
+  }
+
+  disableRemoveFromScore() {
+    const value = this.props.value;
+    const startingValue = Constants.startingValue;
+    
+    if(value <= startingValue) {
+      return "disabled";
+    }
+    return "";
+  }
+
   render() {
-    const value = this.state.value;
+    const value = this.props.value;
     const statName = this.props.statName;
 
     return (
       <div>
         <label>{statName}: {value}</label>
         &nbsp;
-        <button onClick={() => this.addToAttributeScore()}>+</button>
-        <button onClick={() => this.removeFromAttributeScore()}>-</button>
+        <button disabled={this.disableAddToScore()} onClick={() => this.addToAttributeScore()}>+</button>
+        <button disabled={this.disableRemoveFromScore()} onClick={() => this.removeFromAttributeScore()}>-</button>
       </div>
     );
   }
 }
 
-class Builder extends Component {
+class PointBuy extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentPoints: 27
+      currentPoints: Constants.startingPoints
     };
 
     this.updateCurrentPoints = this.updateCurrentPoints.bind(this);
   }
 
-  updateCurrentPoints(decrease, pointCost) {
-    var newPoints;
-
-    if(decrease) {
-      newPoints = (this.state.currentPoints - pointCost);
-    } else {
-      newPoints = (this.state.currentPoints + pointCost);
-    }
+  updateCurrentPoints(pointCost) {
+    var newPoints = (this.state.currentPoints + pointCost);
 
     this.setState({
       currentPoints: newPoints
     });
   }
 
-  renderStat(key, name) {
+  renderStat(key) {
     const currentPoints = this.state.currentPoints;
-    const startingPoints = 27;
-    const startingValue = 8;
-    const maxValue = 15;
+
+    var index = this.props.character.stats.findIndex(obj => obj.key === key);
+    var stat = this.props.character.stats[index];
 
     return (
       <Stat
-        statKey={key}
-        statName={name}
-        startingValue={startingValue}
-        maxValue={maxValue}
-        startingPoints={startingPoints}
+        key={stat.key}
+        value={stat.pointBuyValue}
+        statKey={stat.key}
+        statName={stat.name}
         currentPoints={currentPoints}
         updateCurrentPoints={this.updateCurrentPoints}
+        modifyAttribute={this.props.modifyAttribute}
       />
     );
   }
@@ -138,15 +220,12 @@ class Builder extends Component {
   render() {
     const currentPoints = this.state.currentPoints;
 
+    const pointBuyStats = this.props.character.stats.map((stat) => this.renderStat(stat.key));
+
     return (
       <div>
         <label>Points remaining: {currentPoints}</label>
-        {this.renderStat("STR", "Strength")}
-        {this.renderStat("DEX", "Dexterity")}
-        {this.renderStat("CON", "Constitution")}
-        {this.renderStat("INT", "Intelligence")}
-        {this.renderStat("WIS", "Wisdom")}
-        {this.renderStat("CHA", "Charisma")}
+        {pointBuyStats}
       </div>
     );
   }
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const Constants = {
+    maxValue: 15,
+    startingValue: 8,
+    startingPoints: 27
+}
+
+export default Constants;
