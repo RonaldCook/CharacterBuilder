eb5f06cb043dee6d4f7c41da26011e103789e9a5
RonaldCook <34778211+RonaldCook@users.noreply.github.com>
2017-12-28 13:13:50 -0800

    Adding the RaceSelector component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
 
     this.state = {
       character: {
+        race: "",
         stats: [
           {
             key: 'STR',
@@ -48,24 +49,137 @@ class App extends Component {
             displayValue: Constants.startingValue,
             pointBuyValue: Constants.startingValue
           }
-        ]
+        ],
+        customStatsChosen: 0,
+        customStatsAvalilable: 0,
+        chosenStats: [],
+        excludedStats: []
       }
     };
 
     this.modifyAttribute = this.modifyAttribute.bind(this);
+    this.changeRace = this.changeRace.bind(this);
+    this.modifyAttributeDisplay = this.modifyAttributeDisplay.bind(this);
 
     // Initialize Firebase
     firebase.initializeApp(config.firebase);
   }
 
   modifyAttribute(key, changeValue) {
+    //get the character state
     let character = Object.assign({}, this.state.character);
-    var index = character.stats.findIndex(obj => obj.key === key);
 
-    var stat = character.stats[index];
+    //get the index of the stat to modify
+    const index = character.stats.findIndex(obj => obj.key === key);
+
+    //update the stats display and point buy values
+    let stat = character.stats[index];
     stat.pointBuyValue = (stat.pointBuyValue + changeValue);
     stat.displayValue = (stat.displayValue + changeValue);
 
+    //update the character state
+    this.setState({character: character});
+  }
+
+  changeRace(event) {
+    //get the character state
+    let character = Object.assign({}, this.state.character);
+
+    //update the race
+    character.race = event.target.value;
+
+    //reset ability scores to point buy value when race changes
+    character.stats.map((stat) => {
+      stat.displayValue = stat.pointBuyValue;
+      return stat;
+    });
+    
+    //reset custom scores when race changes
+    character.customStatsAvalilable = 0;
+    character.customStatsChosen = 0;
+    character.chosenStats = [];
+    character.excludedStats = [];
+
+    //grab stat mods from the selected option
+    const selectedIndex = event.target.selectedIndex;
+    const statModsJSON = event.target.options[selectedIndex].getAttribute('statmods');
+    const statMods = JSON.parse(statModsJSON);
+    const customStatsJSON = event.target.options[selectedIndex].getAttribute('customstats');
+    const customStats = JSON.parse(customStatsJSON);
+
+    //if there are any stat mods update the display values to reflect them
+    if(statMods) {
+      statMods.map((stat) => {
+        const index = character.stats.findIndex(obj => obj.key === stat.stat);
+        let characterStat = character.stats[index];
+        characterStat.displayValue = (characterStat.displayValue + stat.mod);
+        return characterStat;
+      });
+    }
+
+    //if there are any custom stat options update display to allow them to be chosen
+    if(customStats) {
+      if(customStats.number > character.customStatsChosen) {
+        //add any stats a race gets already to the exclusion list
+        if(customStats.exclusions) {
+          character.excludedStats.push(customStats.exclusions);
+        }
+
+        //update the custom stats available
+        character.customStatsAvalilable = customStats.number;
+      }
+    }
+    
+    //update the character state
+    this.setState({character: character});
+  }
+
+  disableCustomAdd(key) {
+    //if the character has stats to assign for their race
+    //AND they haven't already assigned an increase to this stat
+    //AND their race doesn't already give a bonus to that stat
+    //let them assign an attribute point
+    if((this.state.character.customStatsAvalilable > this.state.character.customStatsChosen)
+      && (!this.state.character.chosenStats.find(obj => obj === key))
+      && (!this.state.character.excludedStats.find(obj => obj === key))) {
+      return "";
+    }
+    return "disabled";
+  }
+
+  disableCustomRemove(key) {
+    //if the character has stats to assign for their race
+    //AND they have already assigned an increase to this stat
+    //AND their race doesn't already give a bonus to that stat
+    //let them remove the attribute point assigned
+    if((this.state.character.customStatsAvalilable > 0 && this.state.character.customStatsChosen > 0)
+      && (this.state.character.chosenStats.find(obj => obj === key))
+      && (!this.state.character.excludedStats.find(obj => obj === key))) {
+      return "";
+    }
+    return "disabled";
+  }
+
+  modifyAttributeDisplay(index, value) {
+    //get the character state and stat to modify
+    let character = Object.assign({}, this.state.character);
+    let stat = character.stats[index];
+
+    //update custom stat chosen
+    character.customStatsChosen = (character.customStatsChosen + value);
+
+    //if adding, add the stat to the chosenStats list, otherwise remove it
+    if(value > 0) {
+      character.chosenStats.push(stat.key);
+    } else {
+      const chosenStatIndex = character.chosenStats.findIndex(obj => obj === stat.key);
+      character.chosenStats.splice(chosenStatIndex, 1);
+    }
+
+    //update the stats display and point buy values
+    stat.displayValue = (stat.displayValue + value);
+
+    //update the character state
     this.setState({character: character});
   }
 
@@ -77,8 +191,8 @@ class App extends Component {
             <label>{data.name}: </label><input value={data.displayValue} readOnly />
           </div>
           <div>
-            <button disabled={true}>+</button>
-            <button disabled={true}>-</button>
+            <button disabled={this.disableCustomAdd(data.key)} onClick={() => this.modifyAttributeDisplay(index, 1)}>+</button>
+            <button disabled={this.disableCustomRemove(data.key)} onClick={() => this.modifyAttributeDisplay(index, -1)}>-</button>
           </div>
         </td>
       );
@@ -96,7 +210,9 @@ class App extends Component {
           </table>
         </div>
         &nbsp;
-        <PointBuy character={this.state.character} modifyAttribute={this.modifyAttribute}/>
+        <PointBuy stats={this.state.character.stats} modifyAttribute={this.modifyAttribute}/>
+        &nbsp;
+        <RaceSelector race={this.state.character.race} changeRace={this.changeRace} />
       </div>
     );
   }
@@ -121,7 +237,7 @@ class Stat extends Component {
     const currentPoints = this.props.currentPoints;
     const maxValue = Constants.maxValue;
 
-    var pointCost = this.getPointCost(currentValue + 1);
+    const pointCost = this.getPointCost(currentValue + 1);
 
     if((pointCost > 0) && (currentPoints >= pointCost) && (currentValue < maxValue)) {
       this.props.updateCurrentPoints((pointCost * -1));
@@ -135,7 +251,7 @@ class Stat extends Component {
     const startingPoints = Constants.startingPoints;
     const startingValue = Constants.startingValue;
 
-    var pointCost = this.getPointCost(currentValue - 1);
+    const pointCost = this.getPointCost(currentValue - 1);
 
     if((currentPoints < startingPoints) && (currentValue > startingValue) && (pointCost > 0)) {
       this.props.updateCurrentPoints(pointCost);
@@ -165,12 +281,9 @@ class Stat extends Component {
   }
 
   render() {
-    const value = this.props.value;
-    const statName = this.props.statName;
-
     return (
       <div>
-        <label>{statName}: {value}</label>
+        <label>{this.props.statName}: {this.props.value}</label>
         &nbsp;
         <button disabled={this.disableAddToScore()} onClick={() => this.addToAttributeScore()}>+</button>
         <button disabled={this.disableRemoveFromScore()} onClick={() => this.removeFromAttributeScore()}>-</button>
@@ -191,7 +304,7 @@ class PointBuy extends Component {
   }
 
   updateCurrentPoints(pointCost) {
-    var newPoints = (this.state.currentPoints + pointCost);
+    const newPoints = (this.state.currentPoints + pointCost);
 
     this.setState({
       currentPoints: newPoints
@@ -199,10 +312,8 @@ class PointBuy extends Component {
   }
 
   renderStat(key) {
-    const currentPoints = this.state.currentPoints;
-
-    var index = this.props.character.stats.findIndex(obj => obj.key === key);
-    var stat = this.props.character.stats[index];
+    const index = this.props.stats.findIndex(obj => obj.key === key);
+    const stat = this.props.stats[index];
 
     return (
       <Stat
@@ -210,7 +321,7 @@ class PointBuy extends Component {
         value={stat.pointBuyValue}
         statKey={stat.key}
         statName={stat.name}
-        currentPoints={currentPoints}
+        currentPoints={this.state.currentPoints}
         updateCurrentPoints={this.updateCurrentPoints}
         modifyAttribute={this.props.modifyAttribute}
       />
@@ -218,14 +329,51 @@ class PointBuy extends Component {
   }
 
   render() {
-    const currentPoints = this.state.currentPoints;
-
-    const pointBuyStats = this.props.character.stats.map((stat) => this.renderStat(stat.key));
+    const pointBuyStats = this.props.stats.map((stat) => this.renderStat(stat.key));
 
     return (
       <div>
-        <label>Points remaining: {currentPoints}</label>
+        <label>Points remaining: {this.state.currentPoints}</label>
         {pointBuyStats}
+      </div>
+    );
+  }
+}
+
+class RaceSelector extends Component {
+  getRaceOptions(race) {
+    //if the race is a group (Dwarves, Elves, etc) build the option group for the race group
+    if(race.raceGroup) {
+      const subrace = race.subraces.map((race) => this.getRaceOptions(race));
+     
+      return (
+        <optgroup key={race.raceGroup} label={race.raceGroup}>
+          {subrace};
+        </optgroup>
+      );
+    } else { //if the race is a singular choice (Half-Elves, Half-Orcs, etc) build the option for the race
+      return (
+        <option
+          key={race.raceKey}
+          value={race.raceKey}
+          statmods={JSON.stringify(race.statMods)}
+          customstats={JSON.stringify(race.customStats)}>
+          {race.race}
+        </option>
+      );
+    }
+  }
+
+  render() {
+    //build the race options and option groups
+    const races = Constants.races.map((race) => this.getRaceOptions(race));
+
+    return (
+      <div>
+        <select onChange={this.props.changeRace} value={this.props.race}>
+          <option value="">Select a race</option>
+          {races}
+        </select>
       </div>
     );
   }
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,253 @@
 const Constants = {
     maxValue: 15,
     startingValue: 8,
-    startingPoints: 27
+    startingPoints: 27,
+    races: [
+        {
+            race: "Dragonborn",
+            raceKey: "Dragonborn",
+            statMods: [
+                {
+                    stat: "STR",
+                    mod: 2
+                },
+                {
+                    stat: "CHA",
+                    mod: 1
+                }
+            ]
+        },
+        {
+            raceGroup: "Dwarf",
+            subraces: [
+                {
+                    race: "Hill Dwarf",
+                    raceKey: "Hill Dwarf",
+                    statMods: [
+                        {
+                            stat: "CON",
+                            mod: 2
+                        },
+                        {
+                            stat: "WIS",
+                            mod: 1
+                        }
+                    ]
+                },
+                {
+                    race: "Mountain Dwarf",
+                    raceKey: "Mountain Dwarf",
+                    statMods: [
+                        {
+                            stat: "CON",
+                            mod: 2
+                        },
+                        {
+                            stat: "STR",
+                            mod: 1
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            raceGroup: "Elf",
+            subraces: [
+                {
+                    race: "Dark Elf",
+                    raceKey: "Dark Elf",
+                    statMods: [
+                        {
+                            stat: "DEX",
+                            mod: 2
+                        },
+                        {
+                            stat: "CHA",
+                            mod: 1
+                        }
+                    ]
+                },
+                {
+                    race: "High Elf",
+                    raceKey: "High Elf",
+                    statMods: [
+                        {
+                            stat: "DEX",
+                            mod: 2
+                        },
+                        {
+                            stat: "INT",
+                            mod: 1
+                        }
+                    ]
+                },
+                {
+                    race: "Wood Elf",
+                    raceKey: "Wood Elf",
+                    statMods: [
+                        {
+                            stat: "DEX",
+                            mod: 2
+                        },
+                        {
+                            stat: "WIS",
+                            mod: 1
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            raceGroup: "Gnome",
+            subraces: [
+                {
+                    race: "Forest Gnome",
+                    raceKey: "Forest Gnome",
+                    statMods: [
+                        {
+                            stat: "INT",
+                            mod: 2
+                        },
+                        {
+                            stat: "DEX",
+                            mod: 1
+                        }
+                    ]
+                },
+                {
+                    race: "Rock Gnome",
+                    raceKey: "Rock Gnome",
+                    statMods: [
+                        {
+                            stat: "INT",
+                            mod: 2
+                        },
+                        {
+                            stat: "CON",
+                            mod: 1
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            raceGroup: "Halfling",
+            subraces: [
+                {
+                    race: "Lightfoot Halfling",
+                    raceKey: "Lightfoot Halfling",
+                    statMods: [
+                        {
+                            stat: "DEX",
+                            mod: 2
+                        },
+                        {
+                            stat: "CHA",
+                            mod: 1
+                        }
+                    ]
+                },
+                {
+                    race: "Stout Halfling",
+                    raceKey: "Stout Halfling",
+                    statMods: [
+                        {
+                            stat: "DEX",
+                            mod: 2
+                        },
+                        {
+                            stat: "CON",
+                            mod: 1
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            race: "Half-Elf",
+            raceKey: "Half-Elf",
+            statMods: [
+                {
+                    stat: "CHA",
+                    mod: 2
+                }
+            ],
+            customStats: {
+                number: 2,
+                exclusions: "CHA"
+            }
+        },
+        {
+            race: "Half-Orc",
+            raceKey: "Half-Orc",
+            statMods: [
+                {
+                    stat: "STR",
+                    mod: 2
+                },
+                {
+                    stat: "CON",
+                    mod: 1
+                }
+            ]
+        },
+        {
+            raceGroup: "Human",
+            subraces: [
+                {
+                    race: "Default Human",
+                    raceKey: "Default Human",
+                    statMods: [
+                        {
+                            stat: "STR",
+                            mod: 1
+                        },
+                        {
+                            stat: "DEX",
+                            mod: 1
+                        },
+                        {
+                            stat: "CON",
+                            mod: 1
+                        },
+                        {
+                            stat: "INT",
+                            mod: 1
+                        },
+                        {
+                            stat: "WIS",
+                            mod: 1
+                        },
+                        {
+                            stat: "CHA",
+                            mod: 1
+                        }
+                    ]
+                },
+                {
+                    race: "Variant Human",
+                    raceKey: "Variant Human",
+                    customStats: {
+                        number: 2
+                    }
+                }
+            ]
+        },
+        {
+            race: "Tiefling",
+            raceKey: "Tiefling",
+            statMods: [
+                {
+                    stat: "INT",
+                    mod: 1
+                },
+                {
+                    stat: "CHA",
+                    mod: 2
+                }
+            ]
+        }
+    ]
 }
 
 export default Constants;
